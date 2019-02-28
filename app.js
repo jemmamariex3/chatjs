183ca146cc706c6136ffae6f4162073512ea689f
Jemma Marie <jemma.tiongson.420@my.csun.edu>
2019-02-27 16:42:48 -0800

    added menu interface

--- app.js
+++ app.js
@@ -1,36 +1,59 @@
 var app = require('http').createServer(response);
-var fs = require('fs');
 var io = require('socket.io')(app);
+var fs = require('fs');
 const readline = require('readline');
 
+const question = "Select one of the possible options.\nCommand Manual (select options 1-8):\n1) Help\n2) MyIP\n3) MyPort\n4) Connect IP PORT\n5) List IP Peers\n6) Terminate IP\n7) Send IP Message\n8) Exit"
 
-//TODO: take in user input for port and change here
-app.listen(3000);
+var port = process.env.PORT || 3000;
+app.listen(port);
+console.log("App running on port " +port);
 
-//TODO: take user port and output here
-console.log("App running...");
+//Prompts user when the app starts running
+// rl = readline.createInterface(process.stdin, process.stdout),
+// rl.on('line', function(line) {
+//     switch(line.trim()) {
+//         case 1:
+//             console.log('world!');
+//             break;
+//         case 8:
+//             rl.close();
+//             break;
+//         default:
+//             console.log('Say what? I might have heard `' + line.trim() + '`');
+//             break;
+//     }
+//     //shows '>' to let user know they can provide an input
+//     rl.prompt();
+// }).on('close', function() {
+//     console.log('Have a great day!');
+//     process.exit(0);
+// });
+// console.log("\n"+question);
+// rl.prompt();
 
-//TODO: 'help' interface that will call another function to print the other options (callback)
-//function for prompt interface
-function ask(question, callback) {
-    const r = readline.createInterface({
-        input: process.stdin,
-        output: process.stdout});
-    r.question(question + '\n', function(answer) {
-        r.close();
-        callback(null, answer);
-    });
-}
-
-// asks prompt questions on terminal
-ask('Did you find this useful?', function(answer) {
-    console.log(answer)
+rl = readline.createInterface(process.stdin, process.stdout),
+rl.on('line', function(line) {
+    switch(line) {
+        case '1':
+            console.log('world!');
+            break;
+        default:
+            console.log('Say what? I might have heard ' + line);
+            break;
+    }
+    rl.prompt();
+}).on('close', function() {
+    console.log('Have a great day!');
+    process.exit(0);
 });
+console.log("\n"+question);
+rl.prompt();
 
-
+//TODO: Help: sample
 //TODO: myip: figure out how to output laptop IP address
 //TODO: nyport: display port on which this process is listening for incoming connections.
-//TODO: connect <destination> <port no>: establishes a new TCP connection to the specified <destination> at the specified < port no>
+//TODO: connect IP PORT: establishes a new TCP connection to the specified <destination> at the specified < port no>
 //TODO: list: The output should display the IP address and the listening port of all the peers the process is connected to.
 //TODO: terminate <connection id.>: This command will terminate the connection listed under the specified number when LIST is used to display all connections
 //TODO: send <connection id.> <message>: This will send the message to the host on the connection that is designated by the number 3 when command “list” is used.
